7a3a788dea31b62f6dfbdfa28299313d47ca8897
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-10 20:39:23 +0000

    Add controller test cases

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET root' do
+    it 'returns http success' do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
   end
 end
